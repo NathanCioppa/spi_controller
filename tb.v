f36d7fdb8906fe77cca0f37fe1e56ef8e3c40124
Nathan Cioppa <nathan.cioppa@gmail.com>
2026-06-13 12:22:11 -0400

    started a new testbench task

--- tb.v
+++ tb.v
@@ -7,6 +7,8 @@ localparam CLK_FREQ = 50_000_000;
 localparam SPI_FREQ = 1_000_000;
 localparam PERIFERAL_COUNT = 1;
 
+localparam WORD_TIME = (1_000_000_000 / SPI_FREQ)*WORD_SIZE;
+
 reg clk;
 reg [PERIFERAL_COUNT-1:0] sel;
 reg [WORD_SIZE-1:0] in_word_buf;
@@ -54,13 +56,31 @@ perif0 (
 .result(out_perif0_word)
 );
 
+integer i;
 reg [WORD_SIZE-1:0] out_exp;
 always #(1_000_000_000 / CLK_FREQ / 2)
 	clk = ~clk;
 
+initial begin
+	$display("Testing add1_module for all single word patterns ...");
+	for(i=0; i < 2**WORD_SIZE; i=i+1)
+		add1_module_test(i);
+
+
+	$display("Testing all single word patterns ...");
+	for(i=0; i < 2**WORD_SIZE; i=i+1)
+		add1_single_test(i);
+
+	$display("Testing 5 words without delay ...");
+	for(i=0; i < 2**WORD_SIZE; i=i+1)
+		add1_5_test(0);
+
+	$display("DONE");
+	$finish;
+end
+
 task add1_single_test;
 input [WORD_SIZE-1:0] in;
-
 begin
 	out_exp = in + 1;
 	in_word_buf = in;
@@ -83,26 +103,29 @@ begin
 		$display(" ");
 	end
 end
-
 endtask
 
-integer i;
+task add1_5_test;
+input [(WORD_SIZE*5)-1:0] in_words;
+begin
+	in_word_buf = in_words[WORD_SIZE-1:0];
+	start = 1;
+	#(WORD_TIME)
+	// use a shift register instead of i
+	for(i=0; i<5; i=i+1) begin
+		out_exp = in_words[(i*WORD_SIZE)+(WORD_SIZE-1):i*WORD_SIZE] + 1;
+		if(out_word_buf != out_exp) begin
+			$display("FAILED add1_5_test at word index:");
+			$display(i);
+			$display(" ");
+		end
+		in_word_buf = in_words[((i+1)*WORD_SIZE)+(WORD_SIZE-1):(i+1)*WORD_SIZE];
+		#(WORD_TIME)
+	end
 
-initial begin
 
-	$display("Testing add1_module for all single word patterns ...");
-	for(i=0; i < 2**WORD_SIZE; i=i+1)
-		add1_module_test(i);
-
-
-	$display("Testing all single word patterns ...");
-	for(i=0; i < 2**WORD_SIZE; i=i+1)
-		add1_single_test(i);
-
-
-	$display("DONE");
-	$finish;
 end
+endtask
 
 reg [WORD_SIZE-1:0] in_a;
 reg in_ready_a = 0;
@@ -111,7 +134,6 @@ add1_perif #(.DATA_SIZE(WORD_SIZE)) a (.in(in_a), .in_ready(in_ready_a), .result
 
 task add1_module_test;
 input [WORD_SIZE-1:0] in;
-	
 begin
 	in_a = in;
 	in_ready_a = 1;
@@ -124,10 +146,9 @@ begin
 		$display(in);
 		$display("got:");
 		$display(out_a);
+		$display(" ");
 	end
 end
-
-	
 endtask
 
 endmodule
